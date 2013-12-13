cdaa8bda9bca68d68eb1604a0abf7e16006ae226
tady <a.dat.jp@gmail.com>
2013-12-13 10:01:34 +0900

    ancestry gem導入, db migrate

--- db/migrate/20131213005609_add_ancestry_to_tags.rb
+++ db/migrate/20131213005609_add_ancestry_to_tags.rb
@@ -1,5 +1,7 @@
 class AddAncestryToTags < ActiveRecord::Migration
   def change
     add_column :tags, :ancestry, :string
+
+    add_index :tags, :ancestry
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130184007) do
+ActiveRecord::Schema.define(version: 20131213005609) do
 
   create_table "post_tags", force: true do |t|
     t.integer  "post_id",    null: false
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20131130184007) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ancestry"
   end
+
+  add_index "tags", ["ancestry"], name: "index_tags_on_ancestry"
 
   create_table "users", force: true do |t|
     t.string   "name"
