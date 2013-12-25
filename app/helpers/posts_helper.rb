9a8f28d647039898dba73a9884583aa3af3b44ca
tady <a.dat.jp@gmail.com>
2013-12-25 09:37:45 +0900

    tag treeをcache

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,9 @@ module PostsHelper
   # @param {ActiveRecord::Relation} node
   def h_display_tree(node)
     _html = '<ul>'
-    _html << %Q{<li><a href="#{ root_path(q: '#' + node.name) }">#{node.name} <span class="badge">#{node.post_tags.count}</span></a></li>}
+    if node.posts.count > 0
+      _html << %Q{<li><a href="#{ root_path(q: '#' + node.name) }">#{node.name} <span class="badge">#{node.posts.count}</span></a></li>}
+    end
     node.children.each do |_child|
       _html << h_display_tree(_child)
     end
