204a8fc9d204433eaacf7ca7e8e0abf6e53a6019
tady <a.dat.jp@gmail.com>
2014-01-07 10:16:51 +0900

    add rubocop test ignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,6 +69,8 @@ Dir.glob(Rails.root.join('db', 'seeds').to_s + '/*').each do |file_name|
   post.save!
 end
 
+# rubocop:disable LineLength
 Post.first.comments.create(author: user_1, body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
 Post.first.comments.create(author: user_2, body: 'Hooooooo....')
 Post.first.comments.create(author: user_1, body: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')
+# rubocop:enable LineLength
