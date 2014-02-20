c2615229f498918c73e34ef87ced4a189170f95b
tady <a.dat.jp@gmail.com>
2014-02-21 08:55:53 +0900

    環境変数を整理

--- spec/controllers/concerns/rv/mailer_spec.rb
+++ spec/controllers/concerns/rv/mailer_spec.rb
@@ -9,23 +9,23 @@ describe RV::Mailer do
   let(:post) { Post.create title: 'ruby rspec', body: 'This is first espec test: ruby' }
 
   it 'valid' do
-    expect { klass.compose_mail(post, user: alice, to: 'dummy@example.com') }.not_to raise_error(ArgumentError)
+    expect { klass.compose_mail(post, user: alice, to: 'dummy@example.com') }.not_to raise_error
   end
 
   it 'missing post' do
-    expect { klass.compose_mail(nil, user: alice, to: 'dummy@example.com') }.to raise_error(ArgumentError)
+    expect { klass.compose_mail(nil, user: alice, to: 'dummy@example.com') }.to raise_error
   end
 
   it 'missing user' do
-    expect { klass.compose_mail(post, user: nil, to: 'dummy@example.com') }.to raise_error(ArgumentError)
+    expect { klass.compose_mail(post, user: nil, to: 'dummy@example.com') }.to raise_error
   end
 
   it 'missing to' do
-    expect { klass.compose_mail(post, user: alice, to: nil) }.to raise_error(ArgumentError)
+    expect { klass.compose_mail(post, user: alice, to: nil) }.to raise_error
   end
 
   it 'invalid to' do
-    expect { klass.compose_mail(post, user: alice, to: 'invalid.email') }.to raise_error(ArgumentError)
+    expect { klass.compose_mail(post, user: alice, to: 'invalid.email') }.to raise_error
   end
 
 end
