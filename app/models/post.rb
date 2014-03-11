715625b7eee845ca9cb7384150ec17367e054384
tady <a.dat.jp@gmail.com>
2014-03-11 13:57:54 +0900

    user recently post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, through: :post_tags
   belongs_to :author, class_name: 'User'
-  has_many :comments
+  has_many :comments, inverse_of: 'posts'
 
   default_scope  { order(:updated_at => :desc) }
 
