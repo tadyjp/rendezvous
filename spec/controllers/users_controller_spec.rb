36905501208f6ce8dbf8a4ce7786cc15b64c7de8
tady <a.dat.jp@gmail.com>
2014-04-01 14:13:22 +0900

    fix device routing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,20 +2,23 @@ require 'spec_helper'
 
 describe UsersController do
 
+  before do
+  end
+
   describe "GET 'edit'" do
-    pending
-    # it "returns http success" do
-    #   get :edit
-    #   response.should be_success
-    # end
+    login_user
+    it "returns http success" do
+      get :edit
+      response.should be_success
+    end
   end
 
   describe "GET 'update'" do
-    pending
-    # it "returns http success" do
-    #   put :update
-    #   response.should be_success
-    # end
+    login_user
+    it "returns http success" do
+      patch :update, user: { nickname: 'bob' }
+      response.should redirect_to('/user/edit')
+    end
   end
 
 end
