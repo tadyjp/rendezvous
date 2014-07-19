d403739fe371edb9c7eb964e48f7945708a78454
tady <a.dat.jp@gmail.com>
2014-07-19 18:02:23 +0900

    upgrade teaspoon

--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -42,7 +42,7 @@ Teaspoon.configure do |config|
     # Note: If no version is specified, the latest is assumed.
     #
     # Available: jasmine[1.3.1, 2.0.0], mocha[1.10.0, 1.17.1] qunit[1.12.0, 1.14.0]
-    suite.use_framework :jasmine, "1.3.1"
+    suite.use_framework :mocha
 
     # Specify a file matcher as a regular expression and all matching files will be loaded when the suite is run. These
     # files need to be within an asset path. You can add asset paths using the `config.asset_paths`.
@@ -57,7 +57,7 @@ Teaspoon.configure do |config|
     # Note: For CoffeeScript files use `"teaspoon/jasmine"` etc.
     #
     # Available: teaspoon-jasmine, teaspoon-mocha, teaspoon-qunit
-    #suite.javascripts = ["jasmine/1.3.1", "teaspoon-jasmine"]
+    #suite.javascripts = ["mocha/1.17.1", "teaspoon-mocha"]
 
     # You can include your own stylesheets if you want to change how Teaspoon looks.
     # Note: Spec related CSS can and should be loaded using fixtures.
