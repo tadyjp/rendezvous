b271c9f1709a5fb320b9d9888c6411e4f840a25d
tady <tady@zigexn.co.jp>
2014-07-28 23:45:36 +0900

    count方法の修正

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -2,7 +2,5 @@ class FlowController < ApplicationController
 
   def show
     @posts = Post.includes(:tags, :author).where(is_draft: false).order(updated_at: :desc).page(params[:page]).decorate
-    @users = User.all
-    @tags = Tag.all
   end
 end
--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,6 +1,4 @@
 class StockController < ApplicationController
-
   def show
-    @posts = Post.all.count
   end
 end
